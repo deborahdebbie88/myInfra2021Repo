1388ce7a61aa45f74bcc294138ca524c8bb055de
deborahdebbie88 <149524038+deborahdebbie88@users.noreply.github.com>
2024-02-29 22:26:11 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-s3-bucket-for-tf-state"
+    bucket = "my-dev-tf-state-bucket-feb"
     key = "main"
     region = "us-east-2"
     dynamodb_table = "my-dynamodb-table-feb"
